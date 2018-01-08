417acafedcc516f95636cec3ce562943f0735e63
Colin Teal <the30yearswar@hotmail.com>
2018-01-07 22:38:16 -0800

    Update markdown-clj to 1.0.2

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
                  [cljsjs/twemoji              "2.4.0-0"]
                  [com.cognitect/transit-cljs  "0.8.243"]
                  [garden                      "1.3.3"]
-                 [markdown-clj                "1.0.1"
+                 [markdown-clj                "1.0.2"
                   :exclusions [org.clojure/clojure]]
                  [org.clojure/clojure         "1.9.0-alpha19"]
                  [org.clojure/clojurescript   "1.9.671"]
