cbf6e0ec015c384d128d521ef13b6a9fa0fe913f
Colin Teal <the30yearswar@hotmail.com>
2019-02-21 23:13:15 -0800

    Add automatic pause to videos out of viewport

--- src/client/zot_world/core.cljs
+++ src/client/zot_world/core.cljs
@@ -3,12 +3,17 @@
             [goog.dom :as gdom]
             [goog.functions]
             [goog.net.XhrIo :as xhr]
+            [goog.object :as gobj]
             ;; TODO evaluate the need for om-next dependencies
             [om.next :as om]
             [om.dom :as dom]
             [zot-world.components :as components]))
 
 (enable-console-print!)
+
+(extend-type js/NodeList
+  ISeqable
+  (-seq [array] (array-seq array 0)))
 
 (def app-state
   (-> js/document
@@ -64,6 +69,29 @@
                     :Content-Type "application/edn; charset=UTF-8"
                     :X-Requested-With "XMLHttpRequest"}))
      :parser (om/parser {:read read :mutate mutate})}))
+
+(defn visible? [el]
+  (let [rect (.getBoundingClientRect el)]
+    (and
+      (> (gobj/get rect "bottom") 0)
+      (> (gobj/get rect "right") 0)
+      (< (gobj/get rect "left")
+          (gobj/get js/window "innerWidth"))
+      (< (gobj/get rect "top")
+          (gobj/get js/window "innerHeight")))))
+
+(defn playing? [el]
+  (and
+    (> (gobj/get el "currentTime") 0)
+    (false? (gobj/get el "paused"))
+    (false? (gobj/get el "ended"))
+    (> (gobj/get el "readyState") 2)))
+
+(.addEventListener js/window "scroll"
+  #(doseq [video (.querySelectorAll js/document "video")]
+     (when (and (playing? video)
+                (not (visible? video)))
+       (.pause video))))
 
 (om/add-root! reconciler
   components/Posts (gdom/getElement "app"))
