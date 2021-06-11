55113acca8dc2ac935a3cf0741908ba600323266
Colin Teal <the30yearswar@hotmail.com>
2021-06-11 13:59:54 -0700

    Update com.cognitect/transit-cljs to 0.8.269

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
                  [cljsjs/react-dom-server     "16.13.1-0"]
                  [cljsjs/twemoji              "12.1.5-0"]
                  [com.birdduck/hom            "0.1.0-SNAPSHOT"]
-                 [com.cognitect/transit-cljs  "0.8.264"]
+                 [com.cognitect/transit-cljs  "0.8.269"]
                  [garden                      "1.3.10"]
                  [markdown-clj                "1.10.5"
                   :exclusions [org.clojure/clojure]]
