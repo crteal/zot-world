ccfbf3f65201576fe9b9590d4db8d88217080fe6
Colin Teal <the30yearswar@hotmail.com>
2022-12-26 01:10:31 -0800

    Update cider/piggieback to 0.5.3

--- project.clj
+++ project.clj
@@ -57,7 +57,7 @@
     :dev {
       :dependencies [[binaryage/devtools "1.0.6"]
                      [figwheel-sidecar "0.5.20"]
-                     [cider/piggieback "0.5.2"]]
+                     [cider/piggieback "0.5.3"]]
       :source-paths ["src" "dev"]
       :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
       :clean-targets ^{:protect false} ["resources/js/out"
